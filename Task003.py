466fdf7484fd00de08aa6c15209b43ad176a2de1
Roman_Gabdullin <romaga59@gmail.com>
2022-12-17 22:08:28 +0500

    Решил 3 задачу

---
+++ Task003.py
@@ -0,0 +1,25 @@
+# Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, 
+# максимум использование библиотеки Random для получения случайного int
+
+import random
+
+list = []
+
+print('Введите количество элементов списка: ')
+quantity = int(input())
+i = 1
+while i <= quantity:
+    list.append(random.randint(1,10))
+    i+=1
+print(f'Исходный список: {list}')
+
+x = 0
+for j in list:
+    temp = random.randint(1,quantity-1)
+    temp_num = list[temp]
+    list[temp] = j
+    list[x] = temp_num
+    x+=1
+    
+print(f'Результирующий список: {list}')
+
